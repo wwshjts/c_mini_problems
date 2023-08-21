fbb4222f33c4ae68ff995a6860f6d417646fda7c
y.kareba <y.kareba@g.nsu.ru>
2023-08-21 23:19:42 +0700

    Done task 5

---
+++ 5/arr.h
@@ -0,0 +1,7 @@
+void scanArr(int* a, size_t len);
+void printArr(int* a, size_t len);
+void revert(int* a, size_t len);
+int maxArr(int* a, size_t len);
+int findlnArr(int* a, size_t len, int x);
+int extractDigits(int* a, size_t len, int x);
+int compareArrays(int* a, size_t len_a, int* b, size_t len_b);
---
+++ 5/test.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stddef.h>
+#include"arr.h"
+#define SIZE 10 
+
+int main(void){
+	int arr[SIZE];
+	scanArr(arr, SIZE);
+	printArr(arr, SIZE);
+	revert(arr, SIZE);
+	printArr(arr, SIZE);
+	printf("max %d\n", maxArr(arr, SIZE));
+	printf("index of 3 is %d\n", findlnArr(arr, SIZE, 3));
+	int new_arr[10] = {1,2,3,4,5,6,7,8,9,10};
+	printArr(new_arr, 10);
+	extractDigits(new_arr, 10, 331);
+	printArr(new_arr, 10);
+	printf("cmp: %d\n", compareArrays(arr, SIZE, new_arr, 10));
+	return 0;
+}
